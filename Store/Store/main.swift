cb3bd0886e576f6f6245e5389e795f2e8f0886e4
MorVered7 <mvered9@gmail.com>
2025-04-17 14:32:35 -0700

    working on the basics

--- Store/Store/main.swift
+++ Store/Store/main.swift
@@ -7,13 +7,66 @@
 
 import Foundation
 
-protocol SKU {}
+protocol SKU {
+    var name: String { get }
+    func price() -> Int
+}
 
-class Item {}
+class Item: SKU {
+    var name: String
+    var itemPrice: Int
+    init(name: String, priceEach: Int){
+        self.name = name
+        self.itemPrice = priceEach
+    }
+    func price() -> Int {
+        return itemPrice
+    }
+}
 
-class Receipt {}
+//When a Register is created, have it create a Receipt on which to capture all the items scanned
+class Receipt {
+    var list: [SKU]
+    init(list: [SKU] = []) {
+        self.list = list
+    }
+    func items() -> [SKU] {
+        return list
+    }
+    func output() -> String{
+        //print out all of the items stored on the Receipt
+        var result = "Receipt:\n"
+        for item in list {
+            result += "\(item.name): \(item.price())\n"
+        }
+        return result
+    }
+    func total() -> Int {
+        return 0
+    }
+}
 
-class Register {}
+class Register {
+    var receipt: Receipt
+    init(){
+        self.receipt = Receipt()
+    }
+    func scan(_ item: SKU){
+        receipt.list.append(item)
+    }
+    func subtotal() -> Int {
+        var subtotal = 0
+        for item in receipt.list {
+            subtotal += item.price()
+        }
+        return subtotal
+    }
+    func total() -> Receipt {
+        let result = receipt
+        receipt = Receipt(list: [])
+        return result
+    }
+}
 
 class Store {
     let version = "0.1"
@@ -21,4 +74,3 @@ class Store {
         return "Hello world"
     }
 }
-
